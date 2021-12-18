f1d902fbddd0d5efee3777e3a91fed28e90cfa6b
Xiuer0x0 <float800508@hotmail.com>
2021-12-18 22:48:05 +0800

    Fix - test file fullFileName

--- run.js
+++ run.js
@@ -1,5 +1,6 @@
+// test file
 const m3u8 = 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8';
-const fullFileName = `Test-${new Date().valueOf()}`;
+const fullFileName = `Test-${new Date().valueOf()}.mp4`;
 
 const savePathFolder = `${process.env.USERPROFILE}/Downloads/ffmpeg-download`;
 
